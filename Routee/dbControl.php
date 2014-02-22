dd7d9a372de6986551aec97e2c16b53860f2d4ca
jorensorilla <joren.sorilla@gmail.com>
2014-02-22 17:53:36 +0800

    merged map.php from puddley and routee.html

---
+++ Routee/dbControl.php
@@ -0,0 +1,80 @@
+<?php
+
+// database settings 
+$db_username = 'root';
+$db_password = '1234';
+$db_name = 'impassableareas';
+$db_host = 'localhost';
+
+//mysqli
+$mysqli = new mysqli($db_host, $db_username, $db_password, $db_name);
+
+if (mysqli_connect_errno()) 
+{
+    header('HTTP/1.1 500 Error: Could not connect to db!'); 
+    exit();
+}
+
+if($_POST) 
+{    
+    $xhr = $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest'; 
+    if (!$xhr){ 
+        header('HTTP/1.1 500 Error: Request must come from Ajax!'); 
+        exit(); 
+    }
+       
+    $mLatLang   = explode(',',$_POST["latlang"]);
+    $mLat       = filter_var($mLatLang[0], FILTER_VALIDATE_FLOAT);
+    $mLng       = filter_var($mLatLang[1], FILTER_VALIDATE_FLOAT);
+        
+    if(isset($_POST["del"]) && $_POST["del"]==true)
+    {
+        $results = $mysqli->query("DELETE FROM markers WHERE lat=$mLat AND lng=$mLng");
+        if (!$results) {  
+          header('HTTP/1.1 500 Error: Could not delete Markers!');  
+          exit();
+        } 
+        exit("Done!");
+    }
+            
+    $mDesc   = filter_var($_POST["description"], FILTER_SANITIZE_STRING);
+    $mType      = filter_var($_POST["type"], FILTER_SANITIZE_STRING);
+    
+    $results = $mysqli->query("INSERT INTO markers (description, lat, lng, type) VALUES ('$mDesc',$mLat, $mLng, '$mType')");
+    if (!$results) {  
+          header('HTTP/1.1 500 Error: Could not create marker!');  
+          exit();
+    } 
+    
+    $output = '<h3 class="marker-heading">'.$mType.'</h3><p>'.$mDesc.'</p>';
+    exit($output);
+}
+
+
+//Create a new DOMDocument object
+$dom = new DOMDocument("1.0");
+$node = $dom->createElement("markers"); //Create new element node
+$parnode = $dom->appendChild($node); //make the node show up 
+
+// Select all the rows in the markers table
+$results = $mysqli->query("SELECT * FROM markers WHERE 1");
+if (!$results) {  
+    header('HTTP/1.1 500 Error: Could not get markers!'); 
+    exit();
+} 
+
+//set document header to text/xml
+header("Content-type: text/xml"); 
+
+// Iterate through the rows, adding XML nodes for each
+while($obj = $results->fetch_object())
+{
+  $node = $dom->createElement("marker");  
+  $newnode = $parnode->appendChild($node);     
+  $newnode->setAttribute("description", $obj->description);  
+  $newnode->setAttribute("lat", $obj->lat);  
+  $newnode->setAttribute("lng", $obj->lng);  
+  $newnode->setAttribute("type", $obj->type);   
+}
+echo $dom->saveXML();
+?>
