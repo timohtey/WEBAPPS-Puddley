4e7bcfd96f04150e6f5863911d6b3b9a3396c79c
JovenCruz <jovencruz23@gmail.com>
2014-03-19 10:29:45 +0800

    Integerated index beta to index

--- Routee/index.php
+++ Routee/index.php
@@ -1,51 +1,52 @@
-<!DOCTYPE html>
 <html lang = "en">
-    <head>
-        <meta charset="utf-8">
+
+<head>
+	 <meta charset="utf-8">
         <title>Routee</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="">
         <meta name="author" content="">
-        <link href="css/simple.css" rel="stylesheet" type="text/css">
-         <style>
+		
+        <link href="css/cover.css" rel="stylesheet" type="text/css">
+		<link href="css/fonts.css" type = "text/css" rel = "stylesheet">
+		<link href="font-awesome/css/font-awesome.css" rel="stylesheet" type="text/css">
+		
+		<script src="js/jquery-1.10.2.js">
+        </script>
 
-            @font-face{
-                font-family: Custom1;
-                src: url(fonts/YOUARETHEONE.TTF);
-            }
+		<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&libraries=places">
+        </script>
+		
+		 <script type="text/javascript">
+			 $(document).ready(function() {
+		      $("#btnReporting").click(function() {
+					$("#rerouting").fadeOut("fast", function() {
+                    $("#reporting").fadeIn("5000");
+                });
+                });
+            });
 
-
-            body
-            {
-                background-image: url(images/routwall1.jpg);
-                background-repeat:no-repeat;
-                background-size:cover;
-                background-position:center;
-            }
-        </style>
-
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>      
-        <script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyDQFSdn0OTS5bgEVYvfGMBWmkC54uk-6PM&sensor=false"></script>
-        <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&libraries=places&region=ph"></script>
-
-       
-        <script>
+			 $(document).ready(function() {
+		      $("#btnRerouting").click(function() {
+					$("#reporting").fadeOut("fast", function() {
+                    $("#rerouting").fadeIn("5000");
+                });
+                });
+            });
+		</script>
+		<script>
             function initialize() {
                 var sourceInput = document.getElementById('sourceSearchText');
                 var destinationInput = document.getElementById('destinationSearchText');
                 var eventInput = document.getElementById('eventSearchText');
 
-                var autocomplete = new google.maps.places.Autocomplete(sourceInput);
-                var autocomplete2 = new google.maps.places.Autocomplete(destinationInput);
-                var autocomplete3 = new google.maps.places.Autocomplete(eventInput);
+                var autocomplete = new google.maps.places.Autocomplete(sourceInput, {country: 'NL'});
+                var autocomplete2 = new google.maps.places.Autocomplete(destinationSearchText, {country: 'NL'});
+                var autocomplete3 = new google.maps.places.Autocomplete(eventSearchText, {country: 'NL'});
 
                 autocomplete.bindTo('bounds', map);
                 autocomplete2.bindTo('bounds', map);
                 autocomplete3.bindTo('bounds', map);
-
-                autocomplete.setComponentRestrictions({country: 'ph'});
-                autocomplete2.setComponentRestrictions({country: 'ph'});
-                autocomplete3.setComponentRestrictions({country: 'ph'});
 
                 var infowindow = new google.maps.InfoWindow();
 
@@ -68,95 +69,31 @@
             }
             google.maps.event.addDomListener(window, 'load', initialize);
         </script>        
-        <script>
-            $(document).ready(function() {
-                $("#rr").click(function() {
-                    $("#rr").fadeOut();
-                    $("#rp").fadeOut();
-                    $("#route").fadeIn();
-                });
-            });
 
-            $(document).ready(function() {
-                $("#rp").click(function() {
-                    $("#rp").fadeOut();
-                    $("#rr").fadeOut();
-                    $("#report").fadeIn();
-                });
-            });
-
-            $(document).ready(function() {
-                $("#getmrp").click(function() {
-                    $("#rp").fadeIn();
-                    $("#rr").fadeIn();
-                    $("#report").fadeOut();
-                    $('#eventSearchText').val('');
-                    $('#remarksText').val('');
-                    $('#situationText').val('');
-                });
-            });
-
-            $(document).ready(function() {
-                $("#getmrr").click(function() {
-                    $("#rp").fadeIn();
-                    $("#rr").fadeIn();
-                    $("#route").fadeOut();
-                    $('#sourceSearchText').val('');
-                    $('#destinationSearchText').val('');
-                });
-            });
-
-            $(document).ready(function() {
-                $("#contribute").click(function() {
-                    window.location= 'routee.php';
-                });
-
-            });
+</head>
 
 
-        </script>
-
-    </head>
-
-    <body>        
-        <div class="masthead">
-            <h1 align = "center"  style = "font-family:Custom1;font-size:450%;color:#27ae60;">
-                Routee
-            </h1>
-        </div>
-
-        <br>
-        <br>
-        <br>
-
-        <div class = "container">
-
-            <div class = "row">
-                <div class = "col-lg-4">
-                    <button type="button" class="btn btn-primary btn-lg btn-block" id = "rr" style = "font-family:Custom1;font-size:250%;color:#f1c40f;">Reroute</button>
-                </div>
-
-                <div class = "col-lg-4">
-                    <br>
-                    <br>
-                </div>
-
-                <div class = "col-lg-4">
-                    <button type="button" class="btn btn-primary btn-lg btn-block" id = "rp" style = "font-family:Custom1;font-size:250%;color:#f1c40f;">Report</button>
-                </div>
-
-            </div>
-
-            <div class = "row"  style = "display:none;" id = "route">
-
-                <div class ="col-lg-4">
-                </div>
-
-
-                <div class = "col-lg-4">
-                    <div class = "well">
-                        <h2 align = "center" style = "font-family:Custom1;font-size:200%;color:black;"> Rerouting </h2>
-                        <p align = "center"> We'll be helping you in finding the best route possible.</p>
+<body>
+<div class = "container">
+	<div class = "row">
+	<div class = "cw1">		
+		<div class = "col-lg-5">
+			<h2 align="center" id = "me"><img src ="routee.png" align = "center"></h2>
+			<div class = "intros">
+			<p align = "center"> Routee is a re-routing application that will <em>direct</em> you to a brighter tomorrow. 
+			<br><br> If you need assistance, Routee will help you scout out the quickest and most convenient route that will allow you to pass with ease.
+			<br><br> Routee also encourages users to participate in contributing reports regarding what is happening on the road.
+			This ignites the people's willingness to help their fellow drivers and making the road a happier place to travel on.</p>
+			</div>
+		</div>
+		
+		<div class = "col-lg-2">
+		<br><br>
+		</div>
+		<div class = "col-lg-5">
+				<div class = "dissidia" id = "rerouting">
+                <h2 align = "center"> Rerouting </h2>
+				<p align = "center"> Routee will assist you in finding the best route possible.</p>
                         <br>
                         <input id = "sourceSearchText" type="text" class="form-control" placeholder="Where did you come from?">
                         <br/>
@@ -164,65 +101,54 @@
                         <br/>
                         <div class="btn-group btn-group-justified">
                             <div class="btn-group">
-                                <button type="button" class="btn btn-success">Show me the way</button>
-                            </div>
-                            <div class="btn-group">
-                                <button type="button" class="btn btn-danger" id = "getmrr">Cancel</button>
+                                <button type="button" class="btn btn-success"><i class="fa fa-arrows"> </i> Get Directions</button>
                             </div>
                         </div>
-                    </div><!--well end-->
-                </div> <!--col-lg-4 end--> 
+						<hr class = "gdivider">
+						<p align = "center">If there is a situation going on, please do not hesitate to tell us.</p>
+                        <div class="btn-group btn-group-justified">
+                            <div class="btn-group">
+                                <button type="button" class="btn btn-info" id = "btnReporting"><i class="fa fa-microphone"> </i> Reporting</button>
+                            </div>
+                        </div>
+				</div> <!--dissidia rerouting-->
+			
+				<div class ="dissidia" style  = "display:none;" id = "reporting">
+                <h2 align = "center"> Reporting </h2>
+                <p align = "center">Let us know what is going on.</p><br>
+					<form method="POST" action="routee.php">
+                    <select name="pType" class="form-control">
+						<option value="Accident">Accident</option>
+                        <option value="Flood">Flood</option>
+                        <option value="Construction">Construction</option>
+                        <option value="Heavy Traffic">Heavy Traffic</option>
+                        <option value="Others">Others</option>
+                    </select>
+                     <br/>
+                    <input name="placeName" type="text" id = "eventSearchText" class="form-control" placeholder="Where is it happening?" >
+                    <br/>
+                    <textarea style = "width:100%;" placeholder="What's going on?" class="form-control"></textarea>
+                    <br/>                            
+                    <div class="btn-group btn-group-justified">
+                    <div class="btn-group" >                                                       
+						<button type="submit" class="btn btn-success" id="contribute"> <i class="fa fa-microphone"></i> Report</button>
+                     </div>
+					</div> <!-- justified buttons end -->
+					<hr class = "gdivider">					
+					<p align = "center"> If you want to find the fastest way to your destination, just ask us.</p>
+                    <div class="btn-group btn-group-justified">
+                    <div class="btn-group" >                                                       
+						<button type="button" class="btn btn-info" id="btnRerouting"> <i class="fa fa-arrows"></i> Rerouting</button>
+                     </div>
+					</div> <!-- justified buttons end -->					
+                    </form> <!-- form post end -->		
+				</div> <!--dissidia reporting-->
+				
+		</div><!--col-lg-5-end-->	
+	</div><!--cw1-->
+	</div> <!-- row end -->
+</div> <!-- container-end -->
+</body>
 
-                <div class ="col-lg-4">
-                </div>
-
-            </div>
-
-            <div class = "row"  style = "display:none;" id = "report">
-
-                <div class ="col-lg-4">
-                </div>
-
-
-                <div class = "col-lg-4">
-                    <div class = "well">
-                        <h2 align = "center" style = "font-family:Custom1;font-size:200%;color:black;"> Reporting </h2>
-                        <p align = "center">Let us know what is going on.</p><br>
-                        <form method="POST" action="routee.php">
-                            <select name="pType" class="form-control">
-                                <option value="Accident">Accident</option>
-                                <option value="Flood">Flood</option>
-                                <option value="Construction">Construction</option>
-                                <option value="Heavy Traffic">Heavy Traffic</option>
-                                <option value="Others">Others</option>
-                            </select>
-                            <br/>
-                            <input name="placeName" type="text" id = "eventSearchText" class="form-control" placeholder="Where is it happening?" >
-                            <br/>
-                            <input name="pDesc" type="text" id = "remarksText" class="form-control" placeholder="Anything to take note of?">
-                            <br/>                            
-
-                            <div class="btn-group btn-group-justified">
-                                <div class="btn-group" >                                                       
-                                    <button type="submit" class="btn btn-success" id="contribute">Contribute</button>
-                                </div>
-                                <div class="btn-group">
-                                    <button type="button" class="btn btn-danger" id = "getmrp">Cancel</button>
-                                </div>
-                            </div> <!-- justified buttons end -->
-                        </form> <!-- form post end -->
-                    </div><!--well end-->
-                </div> <!--col-lg-4 end--> 
-
-                <div class ="col-lg-4">
-                </div>
-
-            </div>
-
-
-        </div>
-
-
-    </body>
 
 </html>
